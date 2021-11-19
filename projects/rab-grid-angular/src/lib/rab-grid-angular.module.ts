152671469548da2833a91616d406688389f1070d
Rashintha Maduneth <rashinthamaduneth@gmail.com>
2021-11-19 10:54:43 +0530

    Bug fixes

--- projects/rab-grid-angular/src/lib/rab-grid-angular.module.ts
+++ projects/rab-grid-angular/src/lib/rab-grid-angular.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RabGridAngularComponent } from './rab-grid-angular.component';
-
-
+import { FormsModule } from '@angular/forms';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 @NgModule({
   declarations: [
     RabGridAngularComponent
   ],
   imports: [
+    FormsModule,
+    FontAwesomeModule
   ],
   exports: [
     RabGridAngularComponent
   ]
 })
-export class RabGridAngularModule { }
+export class RabGridAngularModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far, fab)
+  }
+}
--- projects/rab-grid-angular/src/lib/rab-grid.enum.ts
+++ projects/rab-grid-angular/src/lib/rab-grid.enum.ts
@@ -1,4 +1,4 @@
-export enum CGridConf {
+export enum RABGridConf {
     Responsiveness,
     Striped,
     Parent,
